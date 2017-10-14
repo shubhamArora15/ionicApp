4b5e2bc1d4878c92cd6ebdbf09954b42d3b66a3c
shubhamarora15 <ashubham.1994@gmail.com>
2017-10-14 09:09:37 +0530

    basic app done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,24 +5,19 @@ angular.module('starter.controllers', [])
   /**
   Log in Section
   */
-  $scope.host = "http://basicapp.arorashubham.com";
+  var hosts = ["http://basicapp.arorashubham.com","http://localhost:3000",'http://192.168.0.107:3000'];
+  $scope.host = hosts[0];
 
   $scope.signin = function(user){
     if(user.email == undefined || user.password == undefined){
       $scope.popUp("Error", "Please fill the all fields");
     }else{
-      // firebase.auth().signInWithEmailAndPassword(user.email, user.password).then(function(result) {
-      //   $scope.popUp("Message","You are Successfully Login");
-      // },
-      // function(error) {
-      //   console.log(error)
-      // });
       $http.post($scope.host + "/login", {user:user})
         .then(function(response){
           if(response.data == "yes"){
               $scope.popUp("message","You Are successfully login");
           }else{
-              $scope.popUp("message","No Data Found");
+              $scope.popUp("message","You are not registered member");
           }
       });
     }
@@ -46,11 +41,6 @@ angular.module('starter.controllers', [])
     if(user == undefined){
       $scope.popUp("error","Please enter email");
     }else{
-      // firebase.auth().sendPasswordResetEmail(user.email).then(function(data) {
-      //   $scope.popUp("Message","You Password reset configuration sent to <b>"+user.email+"</b> Please reset your password ");
-      // }, function(error) {
-      //   $scope.popUp("error","there is some problem in password reseting");
-      // });
       console.log(user);
       $http.post($scope.host + "/resetPassword", {user:user,email:user.emai})
         .then(function(response){
@@ -67,32 +57,20 @@ angular.module('starter.controllers', [])
     if(user==undefined ||user.email == undefined || user.password == undefined || user.phone == undefined){
       $scope.popUp("error","Please fill the fields");
     }else{
-    // firebase.auth().createUserWithEmailAndPassword(user.email, user.password).then(function (result) {
-    //    result.updateProfile({
-    //      displayName: user.name,
-    //      photoURL: "default_dp"
-    //    }).then(function() {}, function(error) {})
-    //      result.sendEmailVerification().then(function(){
-    //         $scope.popUp("Message","Email verification sent to"+ user.email + "Please verify it to sign in")
-    //      });
-    //    }, function (error) {
-    //      $scope.popUp("Warning",error.message)
-    //    });
     $http.post($scope.host+"/users", {user:user})
     .then(function(response){
-        console.log(response.data);
-        // alert("You are successfully registered");
-
-        $scope.popUp("Message", "You Email"+ user.email + "has sent for verification");
-
-        $http.post($scope.host+"/verifyEmail", {user:user})
-        .then(function(response){
-          $http.post($scope.host+"/verify")
-            .then(function(response){
-                console.log(response);
-                $scope.popUp("Success", "You Email"+ user.email + "has successfully verified");
-            });
-        });
+        if(response.data == "already"){
+          $scope.popUp("Message", "You are already memeber!");
+        }else{
+          $scope.popUp("Message", "You Email"+ user.email + "has sent for verification");
+          $http.post($scope.host+"/verifyEmail", {user:user})
+          .then(function(response){
+            $http.post($scope.host+"/verify")
+              .then(function(response){
+                  console.log(response);
+              });
+          });
+        }
     });
 
 
